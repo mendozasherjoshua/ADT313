dd389597847b6596a1a5ff3b5108dd77f818a42c
Sher Joshua Mendoza <147904572+mendozasherjoshua@users.noreply.github.com>
2024-12-05 14:40:36 +0800

    bg login

---
+++ src/pages/Main/Movie/Photos/Photos.jsx
@@ -0,0 +1,7 @@
+const Photos =() =>{
+    return(
+        <>
+         this is photos page create a crud
+        </>
+    )
+}; export default Photos
---
+++ src/pages/Main/Movie/Video/Video.jsx
@@ -0,0 +1,7 @@
+const Videos =() =>{
+    return(
+        <>
+         this is video page create a crud
+        </>
+    )
+}; export default Videos
